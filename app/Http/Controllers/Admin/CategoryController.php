7b04e6c10b3c2b0bd92847adfbfa86f6fe29c042
doidangnam <94689458+doidangnam@users.noreply.github.com>
2022-01-04 17:09:09 +0700

    [Update] Ajax Category

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,12 +3,71 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\CategoryValidator;
+use App\Http\Requests\Admin\UpdateCategoryValidator;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
-    public function categories() 
+    use HasFactory;
+
+    public function index()
     {
         $active = "categories";
-        return view('backend.categories.index', compact('active'));
+        $categories = Category::paginate();
+        return view('backend.categories.index', compact('categories', 'active'));
     }
+
+    public function store(CategoryValidator $request)
+    {
+        $params = $request->all();
+        $slug = Str::slug(data_get($params, 'name'));
+        $newCategory = Category::create([
+            'name' => $params['name'],
+            'description' => $params['description'],
+            'slug' => $slug
+        ]);
+
+        if ($newCategory) {
+            Session::flash('messages_success', 'Great! A Category Is Added');
+            return ['status' => true];
+        }
+
+        return ['status' => false];
+    }
+
+    public function update(UpdateCategoryValidator $request, $id)
+    {
+        $params = $request->all();
+        $slug = Str::slug(data_get($params, 'name'));
+
+        $updatedCategory = Category::where('id', $id)->update([
+            'name' => $params['name'],
+            'description' => $params['description'],
+            'slug' => $slug,
+            'updated_at' => now()
+        ]);
+
+        if ($updatedCategory) {
+            Session::flash('messages_success', 'The category is updated man!');
+            return ['status' => true];
+        }
+
+        return ['status' => false];
+    }
+
+    public function delete($id)
+    {
+        $isDeleted = Category::where('id', $id)->delete();
+
+        if ($isDeleted) {
+            return ['status' => true];
+        }
+
+        return ['status' => false];
+    }
+
 }
